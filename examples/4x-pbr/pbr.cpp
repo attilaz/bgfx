44f376df810067acc9bb435937b893c0dddd7876
Attila Kocsis <kocsisa78@gmail.com>
2019-10-12 22:05:39 +0200

    fixed transform

--- examples/4x-pbr/pbr.cpp
+++ examples/4x-pbr/pbr.cpp
@@ -654,6 +654,8 @@ public:
 				m_uniforms.m_frameParams[ii] = 1.0f;
 			for(uint32_t ii=0; ii<Uniforms::ObjectNumVec4*4; ++ii)
 				m_uniforms.m_objectParams[ii] = 1.0f;
+			m_uniforms.m_skinningEnabled = 0.0f;
+			m_uniforms.m_morphingEnabled = 0.0f;
 			for(uint32_t ii=0; ii<Uniforms::MaterialNumVec4*4; ++ii)
 				m_uniforms.m_materialParams[ii] = 1.0f;
 
@@ -709,20 +711,14 @@ public:
 			bx::memCopy(m_uniforms.m_cameraPos, &m_camera.m_pos.curr.x, 3*sizeof(float) );
 #endif
 
-			// View Transform 0.
-			float view[16];
-			bx::mtxIdentity(view);
-
 			const bgfx::Caps* caps = bgfx::getCaps();
 
-			float proj[16];
-			bx::mtxOrtho(proj, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 100.0f, 0.0, caps->homogeneousDepth);
-			bgfx::setViewTransform(0, view, proj);
-
-			// View Transform 1.
+			// View Transform
+			float view[16];
 			m_camera.mtxLookAt(view);
+			float proj[16];
 			bx::mtxProj(proj, 45.0f, float(m_width)/float(m_height), 0.1f, 100.0f, caps->homogeneousDepth);
-			bgfx::setViewTransform(1, view, proj);
+			bgfx::setViewTransform(0, view, proj);
 
 			// View rect.
 			bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
